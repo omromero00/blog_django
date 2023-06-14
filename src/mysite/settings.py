b9f80f0cd084c3597561c23bb30add9ed5193b01
Omar <oromero@unsa.edu.pe>
2023-06-13 22:44:40 -0500

    Modificando el settings.py

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)#$^f4=hj2dn#q!jt4-l=u6dc)fg9f4q_s=42u0&!a&(@+zit(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/ 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
